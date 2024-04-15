e2298d47c6390e2a7b5ec6c526d588b1db7aa1c2
Ajeet-Singh-2001 <165633291+Ajeet-Singh-2001@users.noreply.github.com>
2024-04-15 21:48:31 +0530

    Add files via upload

--- review.js
+++ review.js
@@ -1,19 +1,50 @@
-const mongoose = require("mongoose");
-const { create } = require("./listing");
-const Schema = mongoose.Schema;
+const express = require("express");
+const router = express.Router({mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require('../models/listing.js');
 
-
-const reviewSchema = new Schema({
-    comment: String,
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    } else {
+        next();
     }
-});
+};
 
-module.exports  = mongoose.model("Review", reviewSchema);
+
+//Post Review Route
+router.post("/", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+
+    console.log("new review saved");
+    res.send("new review saved");
+
+
+}));
+
+//Delete Reviews Route
+router.delete(
+    "/:reviewId",
+    wrapAsync(async (req, res) => {
+        let { id, reviewId } = req.params;
+
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+
+        res.redirect(`/listings/${id}`);
+    })
+);
+
+module.exports = router;
